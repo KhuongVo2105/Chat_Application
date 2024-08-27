26e372f8c3676fb0ccda6cfce4aa43d5ada0b4b0
KhuongVo2105 <khuongvo2105@gmail.com>
2024-08-27 11:59:02 +0700

    create project with 7 layers spring boot

---
+++ ChatApplication/src/main/java/com/chat_application/ChatApplication/Controllers/AuthController.java
@@ -0,0 +1,10 @@
+package com.chat_application.ChatApplication.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+}
---
+++ ChatApplication/src/main/java/com/chat_application/ChatApplication/Controllers/UserController.java
@@ -0,0 +1,9 @@
+package com.chat_application.ChatApplication.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+}
