12551263eda6315b99b9a92a075975e748982deb
KhuongVo2105 <khuongvo2105@gmail.com>
2024-10-02 22:33:03 +0700

    create chat-application

---
+++ chat-application/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}","./app/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        InstagramBold: ['Instagram-Sans-Bold'],
+        InstagramHeadline:['Instagram-Sans-Headline'],
+        InstagramLight:['Instagram-Sans-Light'],
+        Instagram:['Instagram-Sans']
+      }
+    },
+  },
+  plugins: [],
+}
+
