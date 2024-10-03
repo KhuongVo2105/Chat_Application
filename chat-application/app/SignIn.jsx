d84fa1b10902da7a0deff4bbd91cd1a3e89b7083
KhuongVo2105 <khuongvo2105@gmail.com>
2024-10-03 15:18:01 +0700

    created Signin UI

--- chat-application/app/SignIn.jsx
+++ chat-application/app/SignIn.jsx
@@ -1,12 +1,51 @@
 import React from 'react'
-import {View, Text} from 'react-native'
-
+import { View, Text, TouchableOpacity, Image, TextInput } from 'react-native'
+import images from '../contants/image'
+import { useState } from 'react'
 
 const SignIn = () => {
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
+
   return (
 
-    <View className="bg-blue-500">
-      <Text className="text-3xl font-bold underline">Instagram</Text>
+    <View
+      className="w-full h-full flex justify-center items-center bg-white">
+      <View className="w-80 h-auto mb-10">
+        <Image
+          className="w-full h-20"
+          source={images.logo_text} resizeMode='contain' />
+      </View>
+
+      <TextInput
+        className="enabled:hover:border-gray-40 border py-2 px-4 w-96 hover:shadow mb-3 rounded-lg drop-shadow-2xl"
+        onChangeText={setEmail}
+        placeholder='Phone number, username or email address'
+        value={email} />
+      <TextInput
+        className="enabled:hover:border-gray-40 border py-2 px-4 w-96 hover:shadow mb-3 rounded-lg"
+        onChangeText={setEmail}
+        placeholder='Password'
+        value={email} />
+      <TouchableOpacity
+        className="w-96 mb-3">
+        <Text className="text-blue-600 text-right drop-shadow-md">Forgotten password?</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        className="w-96 bg-blue-600 py-3 rounded-lg">
+        <Text className="text-center text-xl font-medium text-white">Log in</Text>
+      </TouchableOpacity>
+
+      <View className="relative justify-center items-center my-7">
+        <View className="w-96 bg-gray-300 h-0.5" />
+        <Text className="absolute m-auto bg-white px-2">Or</Text>
+      </View>
+
+      <TouchableOpacity className="flex flex-row justify-items-center items-center">
+        <Image className="h-6" source={images.icon_fb} resizeMode='contain' />
+        <Text className="text-base text-blue-600">Log in with Facebook</Text>
+      </TouchableOpacity>
     </View>
 
   )
---
+++ chat-application/contants/image.js
@@ -0,0 +1,9 @@
+import logo_text from '../assets/instagram_text.png'
+import splash from '../assets/instagram_splash.jpeg'
+import icon_fb from '../assets/icons--facebook.png'
+
+export default {
+    logo_text,
+    splash,
+    icon_fb
+}
