91901125ecf46be162b0f76f32780e6b77cb557b
nnquy <21130506@st.hcmuaf.edu.vn>
2024-10-04 11:04:04 +0700

    add demo feature update info user

--- ChatApplication/src/main/java/com/chat_application/ChatApplication/Services/UserService.java
+++ ChatApplication/src/main/java/com/chat_application/ChatApplication/Services/UserService.java
@@ -1,7 +1,11 @@
 package com.chat_application.ChatApplication.Services;
 
+import com.chat_application.ChatApplication.Dto.Request.AvatUserReq;
+import com.chat_application.ChatApplication.Dto.Request.InfoUserReq;
 import com.chat_application.ChatApplication.Dto.Response.ApiResponse;
 import com.chat_application.ChatApplication.Dto.Request.CreateUserReq;
+import com.chat_application.ChatApplication.Dto.Response.AvatUserResp;
+import com.chat_application.ChatApplication.Dto.Response.InfoUserResp;
 import com.chat_application.ChatApplication.Entities.User;
 import com.chat_application.ChatApplication.Exceptions.AppException;
 import com.chat_application.ChatApplication.Exceptions.ErrorCode;
@@ -42,5 +46,37 @@ public class UserService {
                 .code(200)
                 .message("User created successfully")
                 .build();
+    }
+
+    public InfoUserResp updateInfoUser(InfoUserReq req) {
+        try {
+            User user = userRepository.findByUsername(req.getUsername()).orElseThrow(() -> new Exception("User not found"));
+            user.setUsername(req.getNewUsername());
+            user.setPrivacy(req.isPrivacy());
+            user.setUpdatedAt(Timestamp.from(Instant.now()));
+            userRepository.save(user);
+
+            return InfoUserResp.builder()
+                    .username(req.getNewUsername())
+                    .privacy(user.isPrivacy())
+                    .build();
+        } catch (Exception e) {
+            throw new RuntimeException("User not exist");
+        }
+    }
+
+    public AvatUserResp updateAvatUser(AvatUserReq req) {
+        try {
+            User user = userRepository.findByUsername(req.getUsername()).orElseThrow(() -> new Exception("User not found"));
+            user.setAvatar(req.getAvatar());
+            userRepository.save(user);
+
+            return AvatUserResp.builder()
+                    .username(user.getUsername())
+                    .avatar(user.getAvatar())
+                    .build();
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
     }
 }
