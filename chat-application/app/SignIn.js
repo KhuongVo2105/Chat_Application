25b18c1e6f03206b17c86125b92bbe138b208ea4
KhuongVo2105 <khuongvo2105@gmail.com>
2024-10-09 10:45:34 +0700

    pause

---
+++ chat-application/app/SignIn.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+
+const SignIn = () => {
+  return (
+    <View className="bg-blue-500">
+      <Text className="text-3xl font-bold underline">Instagram</Text>
+    </View>
+
+  )
+}
+
+export default SignIn
