9b427e1b01cbd6ec7e6c67af93f82d042ce5d47d
nnquy <21130506@st.hcmuaf.edu.vn>
2024-10-09 16:56:37 +0700

    add project serminar by expo

---
+++ ChatAppClient/App.tsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import RegisterScreen from "./component/Register";
+import InfoScreen from "./component/Info";
+import { AuthProvider } from "./component/Context";
+import LoginScreen from "./component/Login"
+import VerifyScreen from "./component/Verify"
+import FontLoader from "./utils/FontLoader";
+import ForgotPassword from "./component/ForgotPassword";
+import VerifyToken from "./component/VerifyToken";
+
+// Tạo Stack Navigator
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <AuthProvider>
+      <FontLoader>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Home">
+            <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="Register" component={RegisterScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="Info" component={InfoScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="Verify" component={VerifyScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="ForgotPassword" component={ForgotPassword} options={{ headerShown: false }} />
+            <Stack.Screen name="VerifyToken" component={VerifyToken} options={{ headerShown: false }} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </FontLoader>
+    </AuthProvider>
+  );
+}
+
---
+++ ChatAppClient/component/Verify.tsx
@@ -0,0 +1,153 @@
+import axios from "axios";
+import React, { useState, useRef, useEffect } from "react";
+import {
+  View,
+  TextInput,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Alert,
+} from "react-native";
+
+import { REACT_APP_API_BASE_URL } from '@env';
+
+export default function VerifyScreen({ route, navigation }) {
+  const [otp, setOtp] = useState(["", "", "", "", "", ""]);
+  const [timer, setTimer] = useState(300); // 5 phút = 300 giây
+  const inputRefs = useRef([]);
+
+  // Lấy username từ route params
+  const { userId } = route.params || {};
+
+  useEffect(() => {
+    const countdown = setInterval(() => {
+      setTimer((prevTimer) => (prevTimer > 0 ? prevTimer - 1 : 0));
+    }, 1000);
+    return () => clearInterval(countdown); // Hủy bỏ bộ đếm khi component unmount
+  }, []);
+
+  const handleOtpChange = (text, index) => {
+    let otpArray = [...otp];
+    otpArray[index] = text;
+    setOtp(otpArray);
+
+    // Tự động di chuyển tới ô tiếp theo sau khi nhập xong
+    if (text && index < 5) {
+      inputRefs.current[index + 1].focus();
+    }
+
+    // Nếu xóa ký tự, quay lại ô trước
+    if (!text && index > 0) {
+      inputRefs.current[index - 1].focus();
+    }
+  };
+
+  const handleSubmit = async () => {
+    const otpCode = otp.join("");
+    const endpoint = `${REACT_APP_API_BASE_URL}/users/verify`;
+    console.log(`Instagram_VerifyToken_endpoint: ${endpoint}`)
+    console.log("userId : ",userId +"otp : ",otpCode)
+    const response = await axios.post(endpoint, 
+      {
+        "userId": userId,
+        "passTemp": otpCode 
+      }
+    )
+    if (response.status == 200) {
+      Alert.alert("Success", "OTP is correct!");
+      navigation.navigate("Login");
+    } else {
+      Alert.alert("Error", "Incorrect OTP code.");
+    }
+  };
+  const handleResendCode = async () => {
+    // const otpCode = otp.join("");
+    // const response = await axios.post("http://192.168.1.6:8080/spring/verify/resend",
+    //   userId
+    // )
+    // if (response.status == 200) {
+    //   Alert.alert("Success", "Resend code successful!");
+    // } else {
+    //   Alert.alert("Error", "Have error wait, please");
+    // }
+  };
+
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return `${minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Verify your OTP</Text>
+      <View style={styles.otpContainer}>
+        {otp.map((value, index) => (
+          <TextInput
+            key={index}
+            style={styles.otpInput}
+            maxLength={1}
+            keyboardType="numeric"
+            value={value}
+            onChangeText={(text) => handleOtpChange(text, index)}
+            ref={(el) => (inputRefs.current[index] = el)} // Gán ref cho mỗi ô input
+          />
+        ))}
+      </View>
+      <Text style={styles.timer}>Time remaining: {formatTime(timer)}</Text>
+      <TouchableOpacity style={styles.submitBtn} onPress={handleResendCode}>
+        <Text style={styles.submitText}>Resend code</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.submitBtn} onPress={handleSubmit}>
+        <Text style={styles.submitText}>Verify</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 80,
+    paddingHorizontal: 20,
+    backgroundColor: "white",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 30,
+    marginBottom: 20,
+    textAlign: "center",
+    fontFamily: "Instagram-Sans",
+  },
+  otpContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 30,
+  },
+  otpInput: {
+    fontSize: 20,
+    borderBottomWidth: 2,
+    borderBottomColor: "gray",
+    width: 40,
+    textAlign: "center",
+    marginHorizontal: 5,
+  },
+  timer: {
+    fontSize: 16,
+    marginBottom: 20,
+    color: "gray",
+  },
+  submitBtn: {
+    backgroundColor: "#0095f6",
+    paddingVertical: 12,
+    paddingHorizontal: 40,
+    borderRadius: 8,
+    alignItems: "center",
+    marginTop:50
+  },
+  submitText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
