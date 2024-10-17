3dfa8ad64e1885e4b1d1ee2ffad170ddacbcd4ff
KhuongVo2105 <khuongvo2105@gmail.com>
2024-10-17 16:09:54 +0700

    commit n change branch to createPost

--- chat-application/app/Home.jsx
+++ chat-application/app/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, Image, TextInput, Alert, ActivityIndicator } from 'react-native';
+import { View, Text, TouchableOpacity, Image, TextInput, Alert, ActivityIndicator, StyleSheet, Pressable, ImageBackground } from 'react-native';
 import images from '../constants/image';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
@@ -16,9 +16,32 @@ const Home = ({ navigation, route }) => {
 
   return (
     <View className="w-full h-full flex justify-center items-center bg-white">
+      <View className="w-full flex flex-row ">
+        <TouchableOpacity>
+          <View className="overflow-hidden" style={styles.story_box}>
+
+            <ImageBackground
+              className="w-full h-full"
+              source={require('./../assets/story_unseen.png')}>
+
+              <Image
+                className="w-full h-full"
+                resizeMode='contain'
+                source={require('./../assets/portaits/portait_1.jpg')}
+              />
+            </ImageBackground>
+          </View>
+        </TouchableOpacity>
+
+      </View>
       <Text>Home page</Text>
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  story_box: {
+    width: 80, height: 80,
+  }
+})
 export default Home;
