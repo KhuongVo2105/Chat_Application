d90a6d240ca9488418a8c3c28d2adae62cb00bd6
nnquy <21130506@st.hcmuaf.edu.vn>
2025-01-02 15:03:49 +0700

    add demo admin web(have some api) + api for user post + follow

--- admin/src/components/admin/AdminHome/AdminHome.tsx
+++ admin/src/components/admin/AdminHome/AdminHome.tsx
@@ -19,53 +19,10 @@ const AdminHome = () => {
   const [clickCategory, setClickCategory] = useState(false);
   const [clickComment, setClickComment] = useState(false);
   const [clickCreateAdmin, setClickCreateAdmin] = useState(false);
-  const [file, setFile] = useState<File | null>(null); // Tệp có thể null ban đầu
-  const [username, setUsername] = useState<string>(''); // Tên người dùng
-  const [message, setMessage] = useState<string>(''); // Thông báo
 
   const handleLogout = () => {
-    // dispatch(logoutCurrentUser());
-    // localStorage.removeItem("authToken");
-  };
-
-  // Hàm xử lý sự kiện khi người dùng chọn file
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const selectedFile = e.target.files?.[0]; // Lấy file người dùng chọn
-    if (selectedFile) {
-      setFile(selectedFile); // Lưu tệp vào state
-    }
-  };
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    if (!file) {
-      setMessage('Please select a file to upload');
-      return;
-    }
-
-    const formData = new FormData();
-    formData.append("file", file); // Thêm tệp vào FormData
-    formData.append("username", username); // Thêm tên người dùng vào FormData
-
-    try {
-      const response = await axios.post(
-        "http://localhost:8080/chat-application/v1/users/updateAvat",
-        formData,
-        {
-          headers: {
-            Authorization: `Bearer eyJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJpbnN0YWdyYW0uY29tIiwic3ViIjoicUBnbWFpbC5jb20iLCJleHAiOjE3MzU5NjQyMDQsImlhdCI6MTczNTk2MDYwNCwic2NvcGUiOiIifQ.pe0ZqTYbrlAJoGivRm2KVjfVdXJLbFAicIlNgpyvYPcbOtz5_U8fF_XE2CrCvLwDhVOqRXsmk6B1FDodtk2QpQ`, // Thêm token xác thực
-            "Content-Type": "multipart/form-data", // Chỉ định loại nội dung là multipart/form-data
-          },
-        }
-      );
-
-      // Nếu upload thành công, hiển thị thông báo
-      console.log(response.data); // In kết quả trả về từ server
-    } catch (error) {
-      // Nếu có lỗi, hiển thị thông báo lỗi
-      console.error(error);
-    }
+    dispatch(logoutCurrentUser());
+    localStorage.removeItem("authToken");
   };
 
   const navigate = useNavigate();
@@ -131,10 +88,7 @@ const AdminHome = () => {
             </Link>
           </li>
           <li onClick={() => createAdmin()}>
-            <Link
-              to="/admin/adminCreate"
-              className={clickCreateAdmin ? "Click" : ""}
-            >
+            <Link to="/admin/adminCreate" className={clickCreateAdmin ? "Click" : ""}>
               <FaUserPlus /> Thêm quản trị viên
             </Link>
           </li>
@@ -143,28 +97,12 @@ const AdminHome = () => {
       <div className="main-content">
         <div className="navbar">
           <h1>
-            Welcome to Admin {currentUser ? currentUser.fullName : ""}
+            Welcome to Admin  {currentUser ? currentUser.fullName : ""}
             <Link to="/login" onClick={handleLogout}>
               Đăng xuất
             </Link>
           </h1>
         </div>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="username">Username: </label>
-            <input
-              type="text"
-              id="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </div>
-          <div>
-            <label htmlFor="file">Upload Avatar: </label>
-            <input type="file" id="file" onChange={handleFileChange} />
-          </div>
-          <button type="submit">Upload</button>
-        </form>
         <div className="content">
           <Outlet />
         </div>
