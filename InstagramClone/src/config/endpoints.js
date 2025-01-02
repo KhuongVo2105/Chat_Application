2af46d626f116a8febdeeb956ce3deec1c377bc6
KhuongVo2105 <khuongvo2105@gmail.com>
2025-01-02 14:50:31 +0700

    update

--- InstagramClone/src/config/endpoints.js
+++ InstagramClone/src/config/endpoints.js
@@ -1,5 +1,5 @@
 // import { REACT_APP_API_BASE_URL } from '@env';
-const SERVER_ADDRESS = `192.168.1.32`
+const SERVER_ADDRESS = `192.168.1.21`
 const REACT_APP_API_BASE_URL = `http://${SERVER_ADDRESS}:8080/chat-application/v1`
 
 const API_BASE_URL  = REACT_APP_API_BASE_URL;
