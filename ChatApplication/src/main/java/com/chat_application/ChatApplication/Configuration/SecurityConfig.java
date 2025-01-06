41441e735ac6170e8a90cec7275e1776d6001096
nnquy <21130506@st.hcmuaf.edu.vn>
2025-01-06 16:53:32 +0700

    update some api for profile

--- ChatApplication/src/main/java/com/chat_application/ChatApplication/Configuration/SecurityConfig.java
+++ ChatApplication/src/main/java/com/chat_application/ChatApplication/Configuration/SecurityConfig.java
@@ -37,8 +37,9 @@ public class SecurityConfig {
             "v1/post/allPostInMonth", "v1/users/alluserInMonth",
             "v1/post/allPostInDay", "v1/users/alluserInDay",
             "v1/post/allPost", "v1/users/alluserNum",
-            "v1/follow/suggestUser", "v1/follow/followers", "v1/follow/following", "v1/follow/follow",
-            "v1/reportPost/getReportPort", "v1/reportPost/sendReportPort"
+            "v1/follow/suggestUser", "v1/follow/followers", "v1/follow/following", "v1/follow", "v1/follow/isFollowing",
+            "v1/reportPost/getReportPort", "v1/reportPost/sendReportPort",
+            "v1/media/getAllImageOfUserId"
 
     }, PUBLIC_ENDPOINTS_GET = {"/v1/verification/verify"};
 
