f960271a26b5b00d00385f94cce955c3fa022f97
NTNBackEnd <nguyennghia163tp@gmail.com>
2025-01-07 21:52:19 +0700

    logout

--- InstagramClone/src/config/endpoints.js
+++ InstagramClone/src/config/endpoints.js
@@ -1,5 +1,5 @@
 // import { REACT_APP_API_BASE_URL } from '@env';
-const SERVER_ADDRESS = `192.168.10.234`;
+const SERVER_ADDRESS = `10.0.245.64`;
 const REACT_APP_API_BASE_URL = `http://${SERVER_ADDRESS}:8080/chat-application/v1`;
 
 const API_BASE_URL  = REACT_APP_API_BASE_URL;
